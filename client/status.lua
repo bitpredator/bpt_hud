721c25439bbd8cbecca5be65d507a75fddfe6c0d
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-06-23 20:14:11 +0200

    chore: initial commit

---
+++ client/status.lua
@@ -0,0 +1,43 @@
+local health = 0
+local armor = 0
+local food = 0
+local thirst = 0
+local posi = "bottom"
+
+ESX = nil
+
+CreateThread(function()
+	ESX = exports["es_extended"]:getSharedObject()
+end)
+
+AddEventHandler('playerSpawned', function()  -- Enable hud only after player spawn
+	CreateThread(function()
+		while true do 
+			Wait(sleep)
+			if IsEntityDead(GetPlayerPed(-1)) then
+				health = 0
+			else
+				health = math.ceil(GetEntityHealth(GetPlayerPed(-1)) - 100)
+			end
+			armor = math.ceil(GetPedArmour(GetPlayerPed(-1)))
+			TriggerEvent('esx_status:getStatus', 'hunger', function(status)
+				food = status.getPercent()
+			end)
+			TriggerEvent('esx_status:getStatus', 'thirst', function(status)
+				thirst = status.getPercent()
+			end)
+			Wait(sleep)
+			SendNUIMessage({
+				posi = posi,
+				show = IsPauseMenuActive(),  -- Disable hud if settings menu is active
+				health = health,
+				armor = armor,
+				food = food,
+				thirst = thirst,
+			})
+		end
+	end)
+end)
+
+
+
---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'bitpredator HUD'
+version '0.0.4'
+
+ui_page 'html/ui.html'
+
+files {
+	'html/*.css',
+	'html/*.js',
+	'html/*.png',
+	'html/ui.html',
+}
+
+
+client_scripts {
+    'client/*.lua',
+	'config.lua'
+}
